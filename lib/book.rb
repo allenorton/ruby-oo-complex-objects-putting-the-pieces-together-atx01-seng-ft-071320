ffb4887342c5366dc3e466e8927677696ca25b96
Allene Norton <norton.allene@gmail.com>
2020-07-14 21:24:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,27 +4,7 @@ class Book
   def initialize(title)
     @title = title
   end 
-  def title
-    @title 
-  end 
-  def author 
-    @author 
-  end 
-  def author=(author_name) 
-    @author= author_name
-  end 
-  def page_count
-    @page_count
-  end
-  def page_count=(num_pages)
-    @page_count= num_pages
-  end 
-  def genre 
-    @genre 
-  end
-  def genre=(genre_name) 
-    @genre = genre_name 
-  end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end 
